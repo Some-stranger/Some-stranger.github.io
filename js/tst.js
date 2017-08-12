d1345b0820629ee4f685f722aa441f1966a5b066
Some-stranger <30572547+Some-stranger@users.noreply.github.com>
2017-08-13 03:47:53 +0400

    New-button

---
+++ js/tst.js
@@ -0,0 +1,5 @@
+$('#ck_phone_widget').hover(
+		function(){ $(this).removeClass('.ck_inactive') }
+		function(){ $(this).addClass('.ck_active' '.ck_green') },
+		function(){ $(this).removeClass('.ck_active' '.ck_green') }
+);
